28a3fa7e04d1cbaae2adc77054a4b1e8a4d107b2
Jimmy Lai <jimmy.lai@codeimmersives.com>
2019-03-27 16:10:18 -0400

    created display function

--- data.js
+++ data.js
@@ -1,3 +1,32 @@
 const data = [
-    
+    {
+        soda: 'Coke',
+        diet: false,
+        sugar: '60mg',
+        calories: '240cal',
+    },
+    {
+        soda:'Sprite',
+        diet: false,
+        sugar: '55mg',
+        calories: '220cal',
+    },
+    {
+        soda: 'Sunkist',
+        diet: false,
+        sugar: '47mg',
+        calories: '230cal',
+    },
+    {
+        soda: 'Diet Coke',
+        diet: true,
+        sugar: '10mg',
+        calories: '110cal'
+    },
+    {
+        soda: 'Dr Pepper',
+        diet: false,
+        sugar: '60mg',
+        calories: '210cal'
+    }
 ]
--- main.js
+++ main.js
@@ -1,1 +1,43 @@
 window.onload = init;
+
+function init(){
+    //runs search function when clicked.
+    document.querySelector('#search')
+        .addEventListener('click', searchButton);
+    document.querySelector('#display-all')
+        .addEventListener('click', displayAll);
+}
+
+function searchButton(event){
+    event.preventDefault();
+    const userInput = document.querySelector('#search-input').value;
+    //check if userinput equals to property value
+   
+    for(let i = 0; i < data.length; i++){
+        if(userInput === data[i].soda);
+        appendToUL();
+    }
+    
+}
+
+function appendToUL(event){
+    event.preventDefault
+    list.appendChild('ul');
+    
+    const newLi = document.createElement('li');
+    newLi.innerText = `Brand: ${data.soda}`
+    ul.appendChild(newLi);
+    
+    const newLi2 = document.createElement('li');
+    newLi2.innerText = `Sugar: ${data.sugar}`
+    ul.appendChild(newLi2);
+
+    const newLi3 = document.createElement('li');
+    newLi3.innerText = `Calories: ${data.calories}`
+    ul.appendChild(newLi3);
+    
+}
+
+function info(){
+    
+}
